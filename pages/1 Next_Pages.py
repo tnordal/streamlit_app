7dca17e59684f5c44bfb58c51774d049e10a343b
Tom Nordal <tnordal@gmail.com>
2023-06-04 16:46:02 +0200

    add next page

---
+++ pages/1 Next_Pages.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+# st.set_page_config(
+#     page_title="Ex-stream-ly Cool App",
+#     page_icon="🧊",
+#     layout="wide",
+#     initial_sidebar_state="expanded",
+#     menu_items={
+#         'Get Help': 'https://www.extremelycoolapp.com/help',
+#         'Report a bug': "https://www.extremelycoolapp.com/bug",
+#         'About': "# This is a header. This is an *extremely* cool app!"
+#     }
+# )
+
+st.title('Next Page')
+
+col1, col2, col3 = st.columns(3, gap='medium')
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
